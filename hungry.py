1d8311396108cf34a4550c8a74a947b5df43867d
vguimet <56273787+vgudimet@users.noreply.github.com>
2020-03-28 20:29:11 -0400

    water

--- hungry.py
+++ hungry.py
@@ -4,4 +4,7 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
+        
